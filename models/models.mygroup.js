be7417aa1187571ad8d4439abc1cc899a0dc4dc0
FusernameC <88580614+FusernameC@users.noreply.github.com>
2023-10-02 22:16:57 +0700

    delete

--- models/models.mygroup.js
+++
@@ -1,7 +0,0 @@
-const mygroup = [ { id: '20110386', name: 'Nguyen Luong Nguyen'},
-                        { id: '20110415', name: 'Vu Hoang Truc Vy'},
-                        { id: '20110371', name: 'Nguyen Van Hon'}];
-
-module.exports = {
-   mygroup
-};
